582045b20a31b494e516933878de277ae98ba097
Sana <sana.niroomand@gmail.com>
2024-01-27 10:58:16 +0330

    first attempt to get commands

--- src/main.c
+++ src/main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 #include "file_functions.h"
+
+void print_command(int argc, char * const argv[]) {
+    for (int i = 0; i < argc; i++) {
+        fprintf(stdout, "%s", argv[i]);
+    }
+    fprintf(stdout, "\n");
+}
 
 //#define _DEB
 
@@ -9,11 +18,18 @@ int main() {
     char *argv[] = {"neogit", "init"};
 
 #else
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
 #endif
-    printf("Hello world!\n");
-    printf("%d\n", argc);
-    return 0;
     
+    if (argc < 2) {
+        perror("please enter a valid command");
+        return 1;
+    }
+
+    if (!strcmp(argv[1], "init")) {
+        print_command(argc, argv);
+    }
+
+    return 0;
 }
